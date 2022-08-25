93c3f9a81b02902b32853bddf95f20f1b4e1df72
carokrny <carolynkrny@gmail.com>
2022-08-25 17:52:02 -0400

    Add Legend component

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import GridWrapper from '../Grid/GridWrapper';
 import Nav from '../Nav';
+import Legend from '../Legend';
 import './App.css';
 
 function App() {
   return (
     <div className="App" id="app">
       <Nav/>
+      <Legend/>
       <GridWrapper/>
     </div>
   );
---
+++ src/Legend/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Legend.css';
+
+function Legend() {
+
+    // return jsx with nav bar
+    return (
+      <div className="legend">
+        <h2>Legend</h2>
+      </div>
+    );
+}
+
+  
+export default Legend;
